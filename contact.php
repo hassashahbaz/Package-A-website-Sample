c2ad2b99a19f293da0e312939c2adf677d739e50
Monkey Mart <hassanshahbazpubggamer@gmail.com>
2026-06-10 13:38:36 +0500

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,8 +1,8 @@
-<?php include 'includes/header.php'; ?>
+<?php include 'header.php'; ?>
 
 <!-- Page Header -->
 <div class="hero" style="height: 50vh;">
-    <div class="hero-bg" style="background-image: url('images/contact_bg.png');" onerror="this.style.backgroundImage='url(\'https://images.unsplash.com/photo-1525610553991-2bede1a236e2?ixlib=rb-4.0.3&auto=format&fit=crop&w=1000&q=80\')'"></div>
+    <div class="hero-bg" style="background-image: url('contact_bg.png');" onerror="this.style.backgroundImage='url(\'https://images.unsplash.com/photo-1525610553991-2bede1a236e2?ixlib=rb-4.0.3&auto=format&fit=crop&w=1000&q=80\')'"></div>
     <div class="hero-overlay"></div>
     <div class="hero-content">
         <h1>Contact Us</h1>
@@ -59,4 +59,4 @@
     </div>
 </section>
 
-<?php include 'includes/footer.php'; ?>
+<?php include 'footer.php'; ?>
